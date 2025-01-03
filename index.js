6f5873e317d1d0902907498460e6462f217c9806
Skraken <hheslaba@gmail.com>
2025-01-03 11:27:20 -0600

    Update js 1.0.0

--- index.js
+++ index.js
@@ -10,40 +10,77 @@ class Animales{
 class Perro extends Animales{
     constructor(nombre, peso, edad, raza){
         super(nombre, peso, edad);
-        this.raza = raza;
+        this._raza = raza;
+    }
+    
+    get raza(){
+        return this._raza;
     }
 
+    set raza(newRaza){
+        this._raza = newRaza;
+    }
+
+    mostrarAnimales () {
+       return `${this.nombre} ${this.peso} ${this.edad} ${this._raza}`;
+    }
 }    
 
 class Gato extends Animales{
     constructor(nombre, peso, edad, sexo){
         super(nombre, peso, edad)
-        this.sexo = sexo;
+        this._sexo = sexo;
+    }
+    get sexo(){
+        return this._sexo;
     }
 
+    set sexo(newSexo){
+        this._sexo = newSexo;
+
+    }
+    mostrarAnimales (){
+       return `${this.nombre} ${this.peso} ${this.edad} ${this._sexo}`;
+    }
+    
 }
 
 class Conejo extends Animales{
     constructor(nombre, peso, edad, color){
         super(nombre, peso, edad)
-        this.color = color;
+        this._color = color;
+    }
+    get color(){
+        return this._color;
+    }
+
+    set color(newColor){
+        this._color = newColor;
+    }
+
+    mostrarAnimales (){
+        return `${this.nombre} ${this.peso} ${this.edad} ${this._color}`;
+    
     }
 }
 
-Animales.prototype.mostrarAnimales = function(){
-    let perrito = `${this.nombre} ${this.peso} ${this.edad}`;
-    let gatito = `${this.nombre} ${this.peso} ${this.edad}`;
-    let conejito = `${this.nombre} ${this.peso} ${this.edad}`;
+//Animales.prototype.mostrarAnimales = function(){
+//    let perrito = `${this.nombre} ${this.peso} ${this.edad} ${this._raza}`;
+//    let gatito = `${this.nombre} ${this.peso} ${this.edad} ${this._sexo}`;
+//    let conejito = `${this.nombre} ${this.peso} ${this.edad} ${this._color}`;
     
-    console.log(perrito, gatito, conejito)
-    return perrito, conejito, gatito;
-}
+//    console.log(perrito, gatito, conejito)
+//    return perrito, conejito, gatito;
+    
+//}
 
-let perro = new Perro("Max", 20, 10, "Doverman");
+
+let perro = new Perro("Max", 20, 10, "Doberman");
 let gato = new Gato("Black", 10, 5, "Macho");
 let conejo = new Conejo("Bad Bonny", 5, 1, "Blanco");
 
 let animales = [perro, gato, conejo];
+console.log(perro)
 
 
 function mostrarAnimales(){
@@ -55,3 +92,4 @@ function mostrarAnimales(){
         container.appendChild(listAnimales);
     }
 }
+
